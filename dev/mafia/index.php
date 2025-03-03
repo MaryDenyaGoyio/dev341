3f7aec8cdfa0793d725316df0095d41d25199668
MaryDenyaGoyio <wjdakffhgksp@kaist.ac.kr>
2025-03-03 18:07:53 +0000

    Initial commit

---
+++ dev/mafia/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <title>간단한 웹 페이지</title>
+</head>
+
+<?php
+  // PHP 코드를 여기에 추가할 수 있습니다.
+?>
+
+<body>
+  <h1>안녕하세요!</h1>
+  <p>이것은 가장 기본적인 PHP 웹 페이지 예제입니다.</p>
+</body>
+</html>
